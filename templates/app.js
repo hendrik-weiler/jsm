9234f2b5090e609afa73641041b6a004ea484d11
Hendrik <hendrik.weiler@gmx.de>
2015-06-24 16:37:04 +0200

    adding view lib

--- templates/app.js
+++ templates/app.js
@@ -5,19 +5,24 @@ App = function(config){
 };
 App.prototype.start = function() {
 	var self = this;
+	var view = new app.View();
 	var router = new app.Router([
 		{
 			route : '#start',
 			controller : new app.Controller(function() {
 				template "start.html" as start;
-				self.config.canvas.innerHTML = start;
+				self.config.canvas.innerHTML = view.render(start,{
+					title : 'Welcome!'
+				});
 			})
 		},
 		{
 			route : '#test',
 			controller : new app.Controller(function() {
 				template "test.html" as test;
-				self.config.canvas.innerHTML = test;
+				self.config.canvas.innerHTML = view.render(test,{
+					
+				});
 			})
 		}
 	]);
