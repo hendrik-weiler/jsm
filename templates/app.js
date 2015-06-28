4459b60a2bb4427f07ce5df6a05592d25ebf4187
Hendrik Weiler <diddlside@googlemail.com>
2015-06-28 10:58:05 +0200

    add base styling

--- templates/app.js
+++ templates/app.js
@@ -4,11 +4,13 @@ App = function(config){
 	this.config = config;
 };
 App.prototype.start = function() {
+	document.title = 'Generated App';
 	var self = this;
 	var view = new app.View();
 	var router = new app.Router([
 		{
 			route : '#start',
+			public : true,
 			controller : new app.Controller(function() {
 				template "start.html" as start;
 				self.config.canvas.innerHTML = view.render(start,{
@@ -18,6 +20,7 @@ App.prototype.start = function() {
 		},
 		{
 			route : '#test',
+			public : false,
 			controller : new app.Controller(function() {
 				template "test.html" as test;
 				self.config.canvas.innerHTML = view.render(test,{
@@ -25,7 +28,10 @@ App.prototype.start = function() {
 				});
 			})
 		}
-	]);
+	], function(cb) {
+		// Authentication
+		cb(true,0); // second parameter determines route index by failed auth redirection
+	});
 	router.check();
 
 };
--- templates/router.js
+++ templates/router.js
@@ -4,8 +4,19 @@ Router = function(routes, authcallback) {
 	this.routes = routes;
 	window.onhashchange = function() {
 		for(var i in routes) {
-			if(routes[i].route == window.location.hash) {
-				routes[i].controller.run();
+			if(routes[i].route == window.location.hash ) {
+				authcallback(function(result, authfailindex) {
+
+					if(routes[i].public) {
+						result = true;
+					}
+
+					if(result) {
+						routes[i].controller.run();
+					} else {
+						routes[authfailindex].controller.run();
+					}
+				});
 			}
 		}
 	}
